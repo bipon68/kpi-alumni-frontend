d308d4f9c5af4854ee00a9146a64d3cbc40b4856
Noman Gazi <nomangazix@gmail.com>
2024-11-22 23:54:09 +0600

    added api fix

---
+++ src/utils/env.ts
@@ -0,0 +1,12 @@
+export const isDev = import.meta.env.MODE === "development";
+
+export const isProd = import.meta.env.MODE === "production";
+
+//--Check if the current environment to set database url
+export const apiUrl = () => {
+  if (isDev && !isProd) {
+    return import.meta.env.VITE_DEV_API_URL;
+  }
+
+  return import.meta.env.VITE_PROD_API_URL;
+};
