02941118e05ecec7ebf34df6476c82181946c483
Pantho Sarkar <pantho.sarkar.7@gmail.com>
2024-11-27 21:52:47 +0600

    Add Manage Event page components and update dependencies
    I have created a table for all event that has been created , we can  manage the events from this table

--- src/app/manage/events/ManageEventPage.tsx
+++ src/app/manage/events/ManageEventPage.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import ManageEventPageHeader from "./components/ManageEventPageHeader";
+import ManageEventPageTableSection from "./components/ManageEventPageTableSection";
 
 const ManageEventPage: React.FC = () => {
-  return <div>ManageEventPage</div>;
+  return (
+    <div className="">
+      <ManageEventPageHeader />
+      <ManageEventPageTableSection />
+    </div>
+  );
 };
 
 export default ManageEventPage;
