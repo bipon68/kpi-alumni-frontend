aab7b73cac0571a04190f20c565421ddc4413527
Anindo Roy <anindoroy112@gmail.com>
2024-12-01 20:37:10 +0600

    MembersGroupSection

--- src/app/manage/members/MembersGroupSection.tsx
+++ src/app/manage/members/MembersGroupSection.tsx
@@ -1,53 +1,63 @@
-import React from "react";
 import { users } from "./Constants";
+import { MembersAddBtnComp } from "./MembersAddBtnComp";
+import { MembersEditBtnComp } from "./MembersEditBtnComp";
+import MembersSearch from "./MembersSearch";
 
 const MembersGroupsSections = () => {
   return (
-    <div className="max-w-5xl mx-auto p-4">
-      <h1 className="text-xl font-bold mb-4">127 Users</h1>
+    <div>
+      <MembersSearch />
+      <div className=" flex w-full items-center justify-between">
+        <h1 className="text-xl font-bold text-secondary ">127 Users</h1>
+        <div className=" m-5">
+          <MembersAddBtnComp />
+        </div>
+      </div>
+
       <div className="overflow-x-auto">
-        <table className="min-w-full table-auto border-collapse border border-gray-200 rounded-lg">
-          <thead>
-            <tr className="bg-gray-200 text-left">
-              <th className="p-3 border border-gray-300">Username</th>
-              <th className="p-3 border border-gray-300">Name</th>
-              <th className="p-3 border border-gray-300">Member Since</th>
-              <th className="p-3 border border-gray-300">Memberships</th>
-              <th className="p-3 border border-gray-300">
-                Membership Statuses
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user, index) => (
-              <tr
-                key={index}
-                className={`${
-                  index % 2 === 0 ? "bg-white" : "bg-gray-100"
-                } hover:bg-gray-50`}
-              >
-                <td className="p-3 flex items-center border border-gray-300">
-                  <img
-                    src={user.image}
-                    alt={user.username}
-                    className="w-10 h-10 rounded-full mr-3"
-                  />
-                  {user.username}
-                </td>
-                <td className="p-3 border border-gray-300">{user.name}</td>
-                <td className="p-3 border border-gray-300">
-                  {user.memberSince}
-                </td>
-                <td className="p-3 border border-gray-300">
-                  {user.memberships}
-                </td>
-                <td className="p-3 border border-gray-300">{user.status}</td>
-              </tr>
+        <div className="min-w-full border-collapse  rounded-lg">
+          <div className="bg-secondary-200 rounded-tl-2xl rounded-tr-2xl flex">
+            {[
+              "Username",
+              "Name",
+              "Member Since",
+              "Memberships",
+              "Membership Statuses",
+              "Action",
+            ].map((header, index) => (
+              <div key={index} className="py-5 text-center font-medium flex-1">
+                {header}
+              </div>
             ))}
-          </tbody>
-        </table>
+          </div>
+          {users.map((user, index) => (
+            <div
+              key={index}
+              className={`flex ${
+                index % 2 === 0 ? "bg-secondary-50" : "bg-secondary-100"
+              } hover:bg-gray-50 cursor-pointer`}
+            >
+              <div className="p-3 flex items-center font-medium flex-1">
+                <img
+                  src={user.image}
+                  alt={user.username}
+                  className="w-10 h-10 rounded-full mr-3"
+                />
+                {user.username}
+              </div>
+              <div className="p-7 text-center flex-1">{user.name}</div>
+              <div className="p-7 text-center flex-1">{user.memberSince}</div>
+              <div className="p-7 text-center flex-1">{user.memberships}</div>
+              <div className="p-7 text-center flex-1">{user.status}</div>
+              <div className="p-7 text-center flex-1">
+                <MembersEditBtnComp />
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
 };
+
 export default MembersGroupsSections;
