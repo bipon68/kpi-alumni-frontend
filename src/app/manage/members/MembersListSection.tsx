e08723954a9e087fadc089749e3689d0b63136d9
Anindo Roy <anindoroy112@gmail.com>
2024-12-01 20:37:24 +0600

    members page update

--- src/app/manage/members/MembersListSection.tsx
+++ src/app/manage/members/MembersListSection.tsx
@@ -1,19 +1,50 @@
 import React from "react";
-import MembersListComp from "./MembersListComp";
+import MembersSearch from "./MembersSearch";
+import { MembersAddBtnComp } from "./MembersAddBtnComp";
+import { users } from "./Constants";
 
 const MembersListSection: React.FC = () => {
   return (
-    <div className="divide-y divide-slate-100">
-      {/* <Nav>
-        <NavItem href="/new" isActive>New Releases</NavItem>
-        <NavItem href="/top">Top Rated</NavItem>
-        <NavItem href="/picks">Vincent’s Picks</NavItem>
-      </Nav> */}
-      <List className="divide-y divide-slate-100">
-        {movies.map((movie) => (
-          <MembersListComp key={movie.id} />
-        ))}
-      </List>
+    <div className="">
+      <MembersSearch />
+      <div className=" flex w-full items-center justify-between">
+        <h1 className="text-xl font-bold text-secondary ">Members</h1>
+        <div className=" m-5">
+          <MembersAddBtnComp />
+        </div>
+      </div>
+
+      <div className="w-full flex gap-3.5">
+        {Array(3)
+          .fill(0)
+          .map((_, index) => (
+            <div
+              key={index}
+              className="w-full border rounded-xl bg-secondary-50 p-4"
+            >
+              <div className="font-medium text-secondary text-lg">
+                Computer Science and Engineering
+              </div>
+              <div className="font-medium text-primary text-sm">
+                Khulna Polytechnic Institute
+              </div>
+              <div className="flex items-center space-x-[-10px] mt-5">
+                {users.slice(0, 3).map((user, index) => (
+                  <div
+                    key={index}
+                    className="relative size-7 border-2 border-white rounded-full overflow-hidden"
+                  >
+                    <img
+                      src={user.image}
+                      alt={user.username}
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
--- src/app/manage/members/MembersPage.tsx
+++ src/app/manage/members/MembersPage.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import MembersGroupsSections from "./MembersGroupSection";
+import MembersListSection from "./MembersListSection";
 
 const MembersPage: React.FC = () => {
-  return <MembersGroupsSections />;
+  return (
+    <div className="max-w-7xl mx-auto p-4 mt-4 rounded-xl h-full">
+      <MembersListSection />
+      {/* <MembersGroupsSections /> */}
+    </div>
+  );
 };
 
 export default MembersPage;
