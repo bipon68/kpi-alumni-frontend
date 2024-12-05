6f5c8bf622031ffffd650a094bc3b0dc2d8b4a09
Pantho Sarkar <pantho.sarkar.7@gmail.com>
2024-12-05 10:29:43 +0600

    Enhance event management UI with improved styling and layout adjustments

--- src/app/manage/events/components/table-comps/TableBodyComp.tsx
+++ src/app/manage/events/components/table-comps/TableBodyComp.tsx
@@ -6,7 +6,7 @@ const TableBodyComp: React.FC = () => {
     return (
         <TableBody>
             {dummyEventData.map((event, index) => (
-                <TableRow key={index}>
+                <TableRow key={index} className="h-[50px] even:bg-primary-50 !border-b-0">
                     <TableHead>img</TableHead>
                     <TableHead>{event.eventId}</TableHead>
                     <TableHead>{event.title}</TableHead>
