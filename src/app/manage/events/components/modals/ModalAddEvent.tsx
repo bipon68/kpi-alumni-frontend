84c68d28335bdebb23fe22f7014b013a881e4a0e
Pantho Sarkar <pantho.sarkar.7@gmail.com>
2024-12-24 10:47:39 +0600

    Implement form handling in ModalAddEvent for event creation

--- src/app/manage/events/components/modals/ModalAddEvent.tsx
+++ src/app/manage/events/components/modals/ModalAddEvent.tsx
@@ -2,12 +2,29 @@ import useModelStore from "@/lib/stores/useModelStore"
 import { Button } from "@/lib/ui/button"
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/lib/ui/dialog"
 import { Input } from "@/lib/ui/input"
+import { useState } from "react"
 
 interface TProps {
     closeModel: () => void
 }
+const defaultFormData = {
+    status: "string",
+    title: "string",
+    description: "string",
+    location: "string",
+    photoUrl: "string"
+}
 
 const ModalBody: React.FC<TProps> = ({ closeModel }) => {
+
+    const [formData, setFormData] = useState(defaultFormData)
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
 
     const handleSubmit = (e: { preventDefault: () => void }) => {
         e.preventDefault()
@@ -19,20 +36,20 @@ const ModalBody: React.FC<TProps> = ({ closeModel }) => {
         <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
             <div className="flex flex-col gap-1">
                 <label htmlFor="picture" className="text-base font-medium text-primary">Add Picture</label>
-                <Input id="picture" type="file" />
+                <Input id="picture" type="file" onChange={handleChange} name="photoUrl" value={formData.photoUrl} />
             </div>
             <div className="flex gap-2">
-                <Input placeholder="Title" />
+                <Input placeholder="Title" name="title" onChange={handleChange} value={formData.title} />
                 {/* to do :  add day picker , day js  or same  kind feature for adding date . */}
-                <Input placeholder="Event Time" />
+                <Input placeholder="Event Time" name="event" />
             </div>
             <div className="flex gap-2">
-                <Input placeholder="Add Location" />
+                <Input placeholder="Add Location" name="location" value={formData.location} />
                 <Input placeholder="Organizer" />
             </div>
             <div className="flex flex-col gap-1">
                 <label htmlFor="description" className="text-base font-medium text-primary">Description</label>
-                <Input className="w-full h-[100px] placeholder:" />
+                <Input className="w-full h-[100px] " name="description" value={formData.description} />
             </div>
             <div className="flex items-center justify-end gap-2">
                 <Button type="button" className=" w-[100px] text-white bg-error" onClick={closeModel}>Cancel</Button>
